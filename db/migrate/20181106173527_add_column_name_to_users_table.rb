94aebe047d081f5a77061782c8212586a7379b95
Lisa Lou <lisa.lou.aeh@gmail.com>
2018-11-06 21:50:45 +0100

    add devise views stufs

---
+++ db/migrate/20181106173527_add_column_name_to_users_table.rb
@@ -0,0 +1,6 @@
+class AddColumnNameToUsersTable < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :name, :string
+    #Ex:- add_column("admin_users", "username", :string, :limit =>25, :after => "email")
+  end
+end
---
+++ db/migrate/20181106174852_remove_nameto_users.rb
@@ -0,0 +1,4 @@
+class RemoveNametoUsers < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
